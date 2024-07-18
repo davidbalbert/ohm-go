e03887f5e59cb9781f168004c62e444507b625b6
David Albert <davidbalbert@gmail.com>
2024-07-18 11:45:15 -0700

    Finish testing literals

--- pexpr_test.go
+++ pexpr_test.go
@@ -2,7 +2,7 @@ package ohm
 
 import "testing"
 
-func literal(s string) PExpr {
+func lit(s string) PExpr {
 	seq := Seq{}
 	for _, r := range s {
 		seq.exprs = append(seq.exprs, &Range{r, r})
@@ -10,19 +10,34 @@ func literal(s string) PExpr {
 	return &seq
 }
 
+type test struct {
+	input   string
+	matches bool
+}
+
 func TestLiteral(t *testing.T) {
 	g := Grammar{
 		super: &BuiltInRules,
 		rules: map[string]PExpr{
-			"start": literal("foo"),
+			"start": lit("foo"),
 		},
 	}
 
-	matches, err := g.MatchesRule("start", "foo")
-	if err != nil {
-		t.Fatal(err)
+	tests := []test{
+		{"foo", true},
+		{"fooo", false},
+		{"fo", false},
 	}
-	if !matches {
-		t.Errorf("\"foo\" should succeed")
+
+	for _, test := range tests {
+		res, err := g.MatchesRule("start", test.input)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if test.matches && !res {
+			t.Errorf("\"%s\" should succeed", test.input)
+		} else if !test.matches && res {
+			t.Errorf("\"%s\" should fail", test.input)
+		}
 	}
 }
